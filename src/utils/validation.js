22b3b632fd512688645fdd97289d38f66a7fac9c
BuyukCem <cembuyuk7@gmail.com>
2020-08-05 14:23:07 +0200

    debug(user): debug update function

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -4,8 +4,8 @@ const Joi = require('@hapi/joi');
 //register Validation
 const registerValidation = data => {
     const schema = {
-        lastname: Joi.string().required().error(new Error('Please insert a name ')),
-        name: Joi.string().required().error(new Error('Please insert a pseudonym ')),
+        lastname: Joi.string().required().error(new Error('Please insert a last name ')),
+        name: Joi.string().required().error(new Error('Please insert a name ')),
         email: Joi.string().min(8).required().email().error(new Error('Please insert a valid email')),
         password: Joi.string().min(8).required().error(new Error('Please insert a password of more than 8 characters'))
     };
@@ -19,7 +19,15 @@ const loginValidation = data => {
     }
     return Joi.validate(data, schema);
 }
-
+const updateUserValidation = data =>{
+    const schema = {
+        lastname: Joi.string().required().error(new Error('Please insert a name lastname')),
+        name: Joi.string().required().error(new Error('Please insert a name ')),
+        email: Joi.string().min(8).required().email().error(new Error('Please insert a valid email')),
+    }
+    return Joi.validate(data, schema);
+}
 
 module.exports.loginValidation = loginValidation;
 module.exports.registerValidation = registerValidation;
+module.exports.updateUserValidation = updateUserValidation;
